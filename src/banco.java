df43e4aa6f0c67ba9e2aa0ae3646ea1df7ac4e48
oteste44 <oteste306@gmail.com>
2026-06-30 17:44:48 -0300

    ui ui ui

---
+++ src/banco.java
@@ -0,0 +1,12 @@
+import herança.conta;
+public class banco {
+    public static void main(String[] args) {
+        conta cc = new conta(1001,1234,"joao",100, true);
+        System.out.println("----------------------------");
+        System.out.println("ContaCorrente");
+        System.out.printf("Saldo Inicial CC: %.2f\n",cc.getSaldo());
+//        cc.depositar(400)
+//        cc.sacar(300)
+        System.out.printf("Saldo Final CC: %.2f", cc.getSaldo());
+    }
+}
---
+++ src/herança/conta.java
@@ -0,0 +1,48 @@
+package herança;
+
+public class conta {
+    private int agencia;
+    private int numeroConata;
+    private String titularConta;
+    private double saldo;
+    private boolean isAtiva;
+
+    public conta(int agencia, int numeroConata, String titularConta, double saldo, boolean isAtiva) {
+        this.agencia = agencia;
+        this.numeroConata = numeroConata;
+        this.titularConta = titularConta;
+        this.saldo = saldo;
+        this.isAtiva = isAtiva;
+
+    }
+
+    public boolean isAtiva() {
+        return isAtiva;
+    }
+
+    public void setAtiva(boolean ativa) {
+        isAtiva = ativa;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getTitularConta() {
+        return titularConta;
+    }
+
+    public int getNumeroConata() {
+        return numeroConata;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void depositar(double valor){this.saldo += valor;}
+
